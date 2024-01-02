1d103ee79c9bbf725d27763e04179b0b8045bcf5
AbooSalh <a.m0507979@gmail.com>
2024-01-02 16:35:50 +0200

    add header

---
+++ main.js
@@ -0,0 +1,29 @@
+const otherBtn = document.querySelector(".other");
+const link = document.querySelectorAll("a");
+const megaMenu = document.querySelector(".mega-menu");
+// Event listener for the "other link" to toggle the mega menu
+
+otherBtn.addEventListener("click", (_) => {
+  megaMenu.classList.toggle("active");
+});
+link.forEach((l) => {
+  l.addEventListener("click", (_) => {
+    megaMenu.classList.remove("active");
+  });
+});
+// Event listener for clicks outside the mega menu and "other link"
+document.addEventListener("click", (event) => {
+  if (
+    !megaMenu.contains(event.target) &&
+    !otherBtn.contains(event.target) &&
+    !event.target.closest(".mega-menu") && // Adjusted condition
+    !megaMenu.isSameNode(event.target)
+  ) {
+    megaMenu.classList.remove("active");
+  }
+});
+
+// Optional: Prevent click propagation within megaMenu
+megaMenu.addEventListener("click", (event) => {
+  event.stopPropagation(); // Or event.stopImmediatePropagation();
+});
