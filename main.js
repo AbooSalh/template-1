f39092dfb9d54c4b54509321f6a27c0ef3f82404
AbooSalh <a.m0507979@gmail.com>
2024-01-03 22:52:44 +0200

    adding landing page

--- main.js
+++ main.js
@@ -2,14 +2,17 @@
 const otherBtn = document.querySelector(".other");
 const link = document.querySelectorAll("a");
 const megaMenu = document.querySelector(".mega-menu");
+const landingContainer = document.querySelector(".landing .container");
 // Event listener for the "other link" to toggle the mega menu
 
 otherBtn.addEventListener("click", (_) => {
   megaMenu.classList.toggle("active");
+  landingContainer.classList.toggle("active");
 });
 link.forEach((l) => {
   l.addEventListener("click", (_) => {
     megaMenu.classList.remove("active");
+    landingContainer.classList.remove("active");
   });
 });
 // Event listener for clicks outside the mega menu and "other link"
@@ -21,6 +24,7 @@ document.addEventListener("click", (event) => {
     !megaMenu.isSameNode(event.target)
   ) {
     megaMenu.classList.remove("active");
+    landingContainer.classList.remove("active");
   }
 });
 
